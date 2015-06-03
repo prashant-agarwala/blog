9ee15367895a0ce74ce8a9747d9a0ed9960c31cc
Anirban Gupta <agupta666@gmail.com>
2015-06-03 20:13:56 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# README
+
+```
+$ git clone <clone-url>
+$ cd blog
+$ bundle install
+$ rake db:create
+$ rake db:migrate
+$ rake cucumber:all
+```
---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,28 @@
+When(/^I go to the new user registration page$/) do
+  visit "/users/sign_up"
+end
+
+When(/^I fill in the new user form with email "(.*?)", password "(.*?)" and password confirmation "(.*?)"$/) do |arg1, arg2, arg3|
+
+
+end
+
+When(/^I submit the form$/) do
+
+end
+
+Then(/^I should be on the dashboard page$/) do
+
+end
+
+Then(/^I should see the 'dashboard' contents$/) do
+
+end
+
+Then(/^I should be on the new user registration page$/) do
+
+end
+
+Then(/^I should see an error message$/) do
+  
+end
